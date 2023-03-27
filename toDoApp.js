54b824ddfeaff99280785dd635218d244a7521af
Nithin <nitinmeena915@gmail.com>
2023-03-27 17:33:09 +0530

    updated to do app

--- toDoApp.js
+++ toDoApp.js
@@ -6,16 +6,20 @@ addBtn.addEventListener("click",function(){
     
     if(input.value == ""){
         input.style.borderColor = 'red';
+        input.setAttribute("placeHolder",'Please enter the task!!');
     }
     else{
         input.style.borderColor = 'green';
+        input.setAttribute("placeHolder",'Enter the task');
+
     let taskDiv = document.createElement('div');
     taskDiv.classList.add("task");
     taskContainer.appendChild(taskDiv);
 
-    let taskText = document.createElement('p');
+    let taskText = document.createElement('input');
+    taskText.type = "text";
     taskText.classList.add("task-input");
-    taskText.innerHTML = input.value;
+    taskText.value = input.value;
     taskDiv.appendChild(taskText);
 
     let checkTask = document.createElement('button');
@@ -26,7 +30,6 @@ addBtn.addEventListener("click",function(){
     let deleteBtn = document.createElement('button');
     deleteBtn.classList.add("delete-btn");
     deleteBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
-    console.log(deleteBtn);
     taskDiv.appendChild(deleteBtn);
 
     input.value = "";
@@ -39,6 +42,13 @@ addBtn.addEventListener("click",function(){
         taskContainer.removeChild(taskDiv);
     })
 }
+})
+
+input.addEventListener("keypress",function(e){
+    if(e.key == "Enter"){
+        e.preventDefault();
+        addBtn.click();
+    }
 })
 
 
